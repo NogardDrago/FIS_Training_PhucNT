3a57dd020be3fa040462e45247db84f8cd531033
NogardDrago <nogarddrago@gmail.com>
2022-06-06 11:12:58 +0700

    1

--- Personal_Project_Take1/src/main/java/fis/java/criminal/enitity/AbstractEntity.java
+++ Personal_Project_Take1/src/main/java/fis/java/criminal/enitity/AbstractEntity.java
@@ -7,4 +7,39 @@ public abstract class AbstractEntity {
     int version;
     LocalDateTime createdAt;
     LocalDateTime modifiedAt;
+
+    public AbstractEntity() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getModifiedAt() {
+        return modifiedAt;
+    }
+
+    public void setModifiedAt(LocalDateTime modifiedAt) {
+        this.modifiedAt = modifiedAt;
+    }
 }
--- Personal_Project_Take1/src/main/java/fis/java/criminal/model/Detective.java
+++ Personal_Project_Take1/src/main/java/fis/java/criminal/model/Detective.java
@@ -5,27 +5,65 @@ import fis.java.criminal.model.CriminalCase;
 import fis.java.criminal.model.enums.EmploymentStatus;
 import fis.java.criminal.model.enums.Rank;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 
 public class Detective extends AbstractEntity {
-    Person person;
+
     String badgeNumber;
     Rank rank;
     Boolean armed;
     EmploymentStatus status;
     Set<CriminalCase> criminalCases;
     Set<TrackEntry> trackEntries;
-
+    String username;
+    String firstName;
+    String lastName;
+    String password;
+    LocalDateTime hiringDate;
     public Detective() {
     }
 
-    public Person getPerson() {
-        return person;
+    public String getUsername() {
+        return username;
     }
 
-    public void setPerson(Person person) {
-        this.person = person;
+    public void setUsername(String username) {
+        this.username = username;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getHiringDate() {
+        return hiringDate;
+    }
+
+    public void setHiringDate(LocalDateTime hiringDate) {
+        this.hiringDate = hiringDate;
+    }
+
 
     public String getBadgeNumber() {
         return badgeNumber;
