45a58e607adec103bc632a2d1e1053dd70dcf964
NogardDrago <nogarddrago@gmail.com>
2022-06-27 12:05:39 +0700

    test

---
+++ Sprint3/src/test/java/com/fis/sprint3/repository/mapper/DetectiveRowMapperTest.java
@@ -0,0 +1,12 @@
+package com.fis.sprint3.repository.mapper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DetectiveRowMapperTest {
+
+    @Test
+    void mapRow() {
+    }
+}
